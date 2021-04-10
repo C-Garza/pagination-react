39560a6bf7a5d9183d613acfbbf5268d158f5b50
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-04-09 18:49:29 -0700

    Set up initial fetch request

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,28 @@
+import {useState, useEffect} from "react";
+
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const url = "https://jsonplaceholder.typicode.com";
+
+  useEffect(() => {
+    fetchPosts(`${url}/posts?_start=${offset}&_limit=${limit}`);
+  }, []);
+
+  const fetchPosts = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPosts(data);
+      return data;
+    }
+    catch(err) {
+      return err;
+    }
+  };
+  console.log(posts);
+
   return(
     <div>HI</div>
   );
