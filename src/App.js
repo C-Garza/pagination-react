5245e748da384e564a3234db2bab83138c37afca
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-04-20 18:01:33 -0700

    Add routing when page and limit is changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from "./App.module.css"
 import Feed from "./components/Feed";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 const App = () => {
   return(
-    <Feed />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Feed} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,18 +3,26 @@ import Posts from "./Posts";
 import styles from "./Feed.module.css";
 import PageNumbers from "./PageNumbers";
 import FeedData from "./FeedData";
+import {useHistory, useLocation} from "react-router-dom";
 
 const Feed = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  let qPage = params.get("page") ? parseInt(params.get("page"), 10) : 1;
+  let qLimit = params.get("limit") ? parseInt(params.get("limit"), 10) : 10;
+
   const [posts, setPosts] = useState([]);
   const [totalPosts, setTotalPosts] = useState(0);
-  const [offset, setOffset] = useState(0);
-  const [limit, setLimit] = useState(10);
+  const [limit, setLimit] = useState(1 <= qLimit <= 100 ? qLimit : 10);
+  const [offset, setOffset] = useState(qPage !== 1 ? ((qPage * limit) - limit) : 0);
   const url = "https://jsonplaceholder.typicode.com";
   const limitOptions = [5,10,50,100];
 
   useEffect(() => {
     // fetchPosts(`${url}/posts?_start=${offset}&_limit=${limit}&_expand=user`); EZ way =P
     fetchPosts(url);
+    history.push({search: `page=${currentPage()}&limit=${limit}`});
   }, [offset, limit]);
 
   const fetchPosts = async (url) => {
